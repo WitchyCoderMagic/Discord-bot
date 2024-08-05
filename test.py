6b4661a63471a93933deabf976077596c0823167
Clover Fairbank <35539288-cloverfairbank@users.noreply.replit.com>
2024-08-05 10:17:15 +0000

    added some print statements

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ import os
 from discord.ext import commands
 #type: ignore
 
+print('Goodbye cruel world')
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
+
+print('Hello world')
