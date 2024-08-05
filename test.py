96c7d150ac65e4c1778b655acec6fbe930fdcd16
Clover Fairbank <35539288-cloverfairbank@users.noreply.replit.com>
2024-08-05 10:18:29 +0000

    got rid of print statements and added a comment

--- test.py
+++ test.py
@@ -3,11 +3,9 @@ import os
 from discord.ext import commands
 #type: ignore
 
-print('Goodbye cruel world')
-
+#establishing intents
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
 
-print('Hello world')
