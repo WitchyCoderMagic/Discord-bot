60ec2ad88b159953babf213b7dc4101388081d59
WitchyCoderMagic <cloverfairbank@yahoo.com>
2024-08-05 11:37:47 +0000

    connected member join event with the server channel id to send a msg on join, same on leave

--- test.py
+++ test.py
@@ -21,10 +21,19 @@ async def hello(ctx):
   await ctx.send("Hi I'm Frenbot, I'm here to help with whatever you need! :3")
 
 
+
 #member join event
 @client.event
 async def on_member_join(member):
-  await member.send('test')
+  channel = client.get_channel(1268253320980598889)
+  await channel.send('Hello')
+
+
+#member leave event
+@client.event
+async def on_member_remove(member):
+  channel = client.get_channel(1268253320980598889)
+  await channel.send('Goodbye')
 
 
 discord_bot_token = os.environ['SECRET_DISCORD']
